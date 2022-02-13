8a55ac37e97a29804d63e519249f8effa20744f9
bravint <bravint@gmail.com>
2022-02-13 14:31:08 +0000

    app.js: mvp complete

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-// const axios = require('axios')
-const matchesRouter = require('./routers/matches');
-const standingsRouter = require('./routers/standings');
-const teamsRouter = require('./routers/teams');
+const axios = require('axios');
 
 const app = express()
 const port = process.env.PORT;
@@ -15,12 +12,24 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.use('/matches', matchesRouter);
-app.use('/standings', standingsRouter);
-app.use('/teams', teamsRouter);
 
-app.get('*', (req, res) => {
-    res.send('Server status: OK');
+app.get('*', async (req, res) => {
+    const endpoint = Object.values(req.params)[0]
+    console.log(endpoint)
+    const url = `${process.env.API_EXT_URL}${endpoint}`;
+    const key = process.env.API_EXT_TOKEN;
+
+    try {
+        const response = await axios({
+            url,
+            headers: {
+                'X-Auth-Token': key,
+            },
+        });
+        res.send(response.data)
+      } catch (error) {
+        res.send(error);
+      }
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
