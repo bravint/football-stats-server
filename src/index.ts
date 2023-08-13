ac68a2d235d04737815e55f6f18ac2911a4a9c3d
bravint <89780339+bravint@users.noreply.github.com>
2023-08-13 21:26:45 +0100

    feat: add rate limit to .env

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ const SERVER_MESSAGES = {
 
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window`
+	max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS!, 10) ?? 100, // Limit each IP to 100 requests per `window`
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
