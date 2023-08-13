608500e9256b5a04cad7a784456bf26d8e63a887
bravint <89780339+bravint@users.noreply.github.com>
2023-08-13 21:17:33 +0100

    feat: add rate-limiting middleware

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import rateLimit from 'express-rate-limit'
 
 import redisClient from './services/redis';
 import { Leagues, Endpoints } from './enums/enums';
@@ -17,10 +18,18 @@ const SERVER_MESSAGES = {
     BAD_REQUEST: 'Invalid id or endpoint',
 };
 
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window`
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
 
 app.get('/:id/:endpoint', async (req, res) => {
     const { id, endpoint } = req.params;
