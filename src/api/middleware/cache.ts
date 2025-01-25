9018c8ded872e0c641d841e1329200947670fe8f
bravint <bravint@gmail.com>
2025-01-25 19:35:13 +0000

    chore: remove log

--- src/api/middleware/cache.ts
+++ src/api/middleware/cache.ts
@@ -1,31 +1,30 @@
-import { Request, Response, NextFunction } from 'express';
+import type { NextFunction, Request, Response } from 'express'
 
-import redisClient from '../../services/redis';
-import { Leagues, Endpoints } from '../../enums/enums';
+import { Endpoints, Leagues } from '../../enums/enums'
+import redisClient from '../../services/redis'
 
 const cacheMiddleware = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-    const { id, endpoint } = req.params;
+	const { id, endpoint } = req.params
 
-    const leagueId = Leagues[id as Leagues];
-    const endpointId = Endpoints[endpoint as Endpoints];
+	const leagueId = Leagues[id as Leagues]
+	const endpointId = Endpoints[endpoint as Endpoints]
 
-    if (!leagueId || !endpointId) {
-        return res.status(400).json('bad request');
-    }
+	if (!leagueId || !endpointId) {
+		return res.status(400).json('bad request')
+	}
 
-    const redisKey = `${leagueId}-${endpointId}`;
-    const cachedData = await redisClient.get(redisKey);
+	const redisKey = `${leagueId}-${endpointId}`
+	const cachedData = await redisClient.get(redisKey)
 
-    if (cachedData) {
-        console.log('cache hit - returning data');
-        return res.status(200).json(JSON.parse(cachedData));
-    }
+	if (cachedData) {
+		return res.status(200).json(JSON.parse(cachedData))
+	}
 
-    next();
-};
+	next()
+}
 
-export default cacheMiddleware;
+export default cacheMiddleware
